85dc37d64f4fa2a22e53252bff0f39d71eba9d10
creagx <arunkumar.m@creagx.com>
2018-02-21 12:23:23 +0530

    Initial Commit

---
+++ Music App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Music App
+//
+//  Created by CREAGX on 21/02/18.
+//  Copyright © 2018 SRIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
